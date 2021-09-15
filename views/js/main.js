00528048bfc412d124b28414f26d1617e1e2bac2
Gabriel de Tassigny <gabriel@multivac.cc>
2021-09-15 10:14:42 +0200

    Calculate scroll top division only once

--- views/js/main.js
+++ views/js/main.js
@@ -504,9 +504,10 @@ function updatePositions() {
   var items = document.querySelectorAll('.mover');
   // document.body.scrollTop is no longer supported in Chrome.
   var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+  scrollTop = scrollTop / 1250;
 
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((scrollTop / 1250) + (i % 5));
+    var phase = Math.sin(scrollTop + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
